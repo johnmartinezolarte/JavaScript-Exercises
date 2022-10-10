bd1e70cf784ad4b37d20a1fcfc9e5a8e161a7e5e
John Martinez <johnmartinezolarte@gmail.com>
2022-10-10 15:20:47 -0500

    more

--- main.js
+++ main.js
@@ -1,31 +1,24 @@
 // EXERCISE 1
 
 /* let slcBox=document.getElementById('colors');
-
 function changeColor(){ */
     /* let slcBox=document.getElementById('colors'); */ /* para cuando sea llamada desde el HTML*/
 
 /*     document.getElementById('number').value=slcBox.selectedIndex;
-
     document.getElementById('text1').value=slcBox.options[slcBox.selectedIndex].text;
-
     document.getElementById('text2').value=slcBox.options[slcBox.selectedIndex].value; */
     /* document.getElementById('text2').value=slcBox.value; */
 
  /*    let paintColor=slcBox.options[slcBox.selectedIndex].value;
-
     document.getElementById('value-color').value=('#'+paintColor);
 }
-
 slcBox.addEventListener('change',changeColor); */
 
 
 // EXERCISE 2
 
 /* const btnValidate2=document.getElementById('btnTextArea');
-
 btnValidate2.addEventListener('click', characterControl);
-
 function characterControl(){
     let txtArea=document.getElementById('description');
     if(txtArea.value.length==0){
@@ -50,7 +43,6 @@ let letterPattern1=/^[a-zA-Z]/;
 let vocalPattern=/[aeiouAEIOUáéíóú]/;
 let txt1='--> El texto comienza con Letra y contiene Vocal <--';
 let txt2='--> El texto NO comienza con Letra o NO contiene Vocal <--';
-
 function validateLetters(){
     let letter=document.getElementById('text3-1').value;
     if(letterPattern1.test(letter) && vocalPattern.test(letter)){
@@ -59,14 +51,11 @@ function validateLetters(){
         display31.innerHTML=txt2;
     }
 }
-
 btn31.addEventListener('click', validateLetters);
-
 let letterPattern2=/^[a-zA-Z0-9]*$/;
 let numberPattern1=/[0-9]{1}/;
 let txt3='--> El texto solo contiene caracteres alfanuméricos y posee 6 caracteres o más <--';
 let txt4='--> El texto NO solo contiene caracteres alfanuméricos ó NO posee 6 caracteres o más <--';
-
 function validateLettersNumbers(){
     let letterNumber=document.getElementById('text3-2').value;
     if(letterPattern2.test(letterNumber) && numberPattern1.test(letterNumber) && letterNumber.length>5){
@@ -75,14 +64,10 @@ function validateLettersNumbers(){
         display32.innerHTML=txt4;
     }
 }
-
 btn32.addEventListener('click', validateLettersNumbers);
-
 let numberPattern2=/^607\d{7}$/;
-
 let txt5='--> El número inicia con 607 y contiene 10 digitos <--';
 let txt6='--> El número NO inicia con 607 ó NO contiene 10 digitos <--';
-
 function validateNumbers(){
     let numbers=document.getElementById('text3-3').value;
     if(numberPattern2.test(numbers)){
@@ -91,7 +76,6 @@ function validateNumbers(){
         display33.innerHTML=txt6;
     }
 }
-
 btn33.addEventListener('click', validateNumbers); */
 
 
@@ -106,12 +90,10 @@ const first=tag('first');
 const second=tag('second');
 const third=tag('third');
 const fourth=tag('fourth');
-
 first.addEventListener('click', ()=>{ muestra('first')})
 second.addEventListener('click', ()=>{ muestra('second')})
 third.addEventListener('click', ()=>{ muestra('third')})
 fourth.addEventListener('click', ()=>{ muestra('fourth')})
-
 function muestra(num){
     let imagen=document.images[num].src;
     let grande=document.images['big'];
@@ -124,29 +106,23 @@ function muestra(num){
 /* const nuevoElemento=document.createElement('p');
 const nuevoTexto=document.createTextNode('Insetando nuevo texto - coex');
 const nuevoNodo=nuevoElemento.appendChild(nuevoTexto);
-
 function fnCrear(){
     let contenedor=document.getElementById('nuevoTexto');
     contenedor.appendChild(nuevoNodo);
 }
-
 function copiar(){
     let copiarNodo= document.getElementById('nuevoTexto');
     copia=copiarNodo.cloneNode(true);
     document.getElementById('copia').appendChild(copia);
 }
-
 function eliminar(){
     let eliminarNodo= document.getElementById('nuevoTexto');
     eliminarNodo.parentNode.removeChild(eliminarNodo);
 }
-
 let btneliminar=document.getElementById('eliminar');
 btneliminar.addEventListener('click', eliminar);
-
 let btncopiar=document.getElementById('duplicar');
 btncopiar.addEventListener('click', copiar);
-
 let btncrear=document.getElementById('crear');
 btncrear.addEventListener('click', fnCrear); */
 
@@ -154,14 +130,14 @@ btncrear.addEventListener('click', fnCrear); */
 // EXERCISE 7
 
 
-let listaElementos =document.querySelector('elementos');
+let listaElementos =document.querySelector('#elementos');
 let form=document.querySelector('#frmLista');
-form.addEventListener('submit', fnAgregarElementos)
+form.addEventListener('submit', fnAgregarElementos);
 
 listaElementos.addEventListener('click', fnEliminarElementos);
 
 
-function fnAgregarElementos(){
+function fnAgregarElementos(evento){
     evento.preventDefault();
     let newElement=document.getElementById('txtElemento').value;
 
@@ -176,3 +152,11 @@ function fnAgregarElementos(){
     li.appendChild(btnDelete)
 }
 
+function fnEliminarElementos(evento){
+    if(evento.target.classList.contains('delete')){
+        if(confirm('¿Esta seguro de eliminar elemento?')){
+            let li=evento.target.parentElement;
+            listaElementos.removeChild(li);
+        }
+    }
+}
